2d45c9d28130bfd851975b70352281cffc696a38
Abbas <aobuya3@gmail.com>
2022-09-02 06:56:37 +0300

    added typescripts imports

--- www/server.js
+++ www/server.js
@@ -1,10 +1,11 @@
+
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
         function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+        step((ngenerator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
 var __importDefault = (this && this.__importDefault) || function (mod) {
@@ -39,6 +40,8 @@ const util_1 = require("./util/util");
     // Root Endpoint
     // Displays a simple message to the user
     //get filtered image
+    
+
     app.get("/filteredimage", (req, res) => __awaiter(this, void 0, void 0, function* () {
         let { image_url } = req.query;
         if (!image_url) {
